c2e3f13df16dfc7253b7fedc055dd6e67d8ba049
lourencomacuacua <ressureicaomacuacua27@gmail.com>
2025-01-13 10:57:44 -0800

    olá mundo!

---
+++ android/app/src/main/kotlin/com/example/finaceiro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.finaceiro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:finaceiro/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
